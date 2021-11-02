09c532ef9020c9b10ef0c7f16eff0a6f0c9c828d
iwasa <Tak1wa@users.noreply.github.com>
2021-11-03 06:27:46 +0900

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace sample_netcore_31
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World!!!");
                 });
             });
         }
